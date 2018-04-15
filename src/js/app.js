9a5df5748da069ff91ff7796cce988413640fb64
David <davidvanochten@hotmail.com>
2018-04-15 12:18:59 +1000

    New branch

--- src/js/app.js
+++ src/js/app.js
@@ -3,18 +3,20 @@ import Router from './lib/Router.js';
 import { createServiceWorker } from './utils/createServiceWorker.js';
 
 const App = (() => {
-  console.log('[App] Service Worker disabled > JS/CSS still disabled');
+  console.log('[App] Service Worker enabled > JS/CSS still disabled');
   createServiceWorker();
 
   window.addEventListener('DOMContentLoaded', e => document.body.classList.add('u-isUnscrollable'));
 
   window.addEventListener('load', () => {
+    // Add splash screen directly inline.
     const SPLASH = document.querySelector('[data-splash]');
     SPLASH.addEventListener('transitionend', e => e.target.parentNode.removeChild(e.target));
 
     setTimeout(() => {
       SPLASH.classList.remove('splash--isVisible');
       document.body.classList.remove('u-isUnscrollable');
+
       Router.init();
       Menu.init();
     }, 1500);
